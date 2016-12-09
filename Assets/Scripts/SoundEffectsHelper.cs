a33180183828b99470633414d8f6b24572a1d45c
Yannis Attard <yannis.attard@gmail.com>
2016-12-09 16:44:46 +0100

    Added soundseffectshelper

---
+++ Assets/Scripts/SoundEffectsHelper.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffectsHelper : MonoBehaviour
+{
+
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public static SoundEffectsHelper Instance;
+
+    public AudioClip explosionSound;
+
+    void Awake()
+    {
+        // Register the singleton
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple instances of SoundEffectsHelper!");
+        }
+        Instance = this;
+    }
+
+    public void MakeExplosionSound()
+    {
+        MakeSound(explosionSound);
+    }
+
+    /// <summary>
+    /// Play a given sound
+    /// </summary>
+    /// <param name="originalClip"></param>
+    private void MakeSound(AudioClip originalClip)
+    {
+        // As it is not 3D audio clip, position doesn't matter.
+        AudioSource.PlayClipAtPoint(originalClip, transform.position);
+    }
+}
