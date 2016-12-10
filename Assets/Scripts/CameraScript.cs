e8f748fb20f353adcd083bd74d5bccb230a6efd9
Yannis Attard <yannis.attard@gmail.com>
2016-12-10 13:42:09 +0100

    Spirit, are you there ?

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class CameraScript : MonoBehaviour {
+
+    private Vector3[] viewPositions = new Vector3[4];
+
+    private int currentView;
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] views = new GameObject[4];
+        views[0] = GameObject.Find("FirstView");
+        views[1] = GameObject.Find("SecondView");
+        views[2] = GameObject.Find("ThirdView");
+        views[3] = GameObject.Find("FourthView");
+
+        for(int i = 0; i < 4; ++i)
+        {
+            SpriteRenderer sprite = views[i].GetComponent<SpriteRenderer>();
+            Vector3 view = new Vector3(views[i].transform.position.x + (sprite.bounds.size.x / 2),
+                                        views[i].transform.position.y + (sprite.bounds.size.y / 2), -10.0f);
+            viewPositions[i] = view;
+        }
+
+        Camera.main.transform.position = viewPositions[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void GoRight()
+    {
+        ResetSelected();
+
+        currentView++;
+        if(currentView >= 3)
+        {
+            currentView = 0;
+        }
+
+        Vector3 nextView = new Vector3(viewPositions[currentView].x, viewPositions[currentView].y, -10.0f);
+        Camera.main.transform.position = nextView;
+    }
+
+    public void GoLeft()
+    {
+        ResetSelected();
+
+        currentView--;
+        if (currentView < 0)
+        {
+            currentView = 2;
+        }
+
+        Vector3 nextView = new Vector3(viewPositions[currentView].x, viewPositions[currentView].y, -10.0f);
+        Camera.main.transform.position = nextView;
+    }
+
+    public void GoDown()
+    {
+        Camera.main.transform.position = viewPositions[3];
+        GameObject.Find("MainInterface/NavigationButtons/Left").SetActive(false);
+        GameObject.Find("MainInterface/NavigationButtons/Right").SetActive(false);
+        GameObject.Find("MainInterface/NavigationButtons/Down").SetActive(false);
+        GameObject.Find("MainInterface/NavigationButtons/Up").SetActive(true);
+    }
+
+    public void GoUp()
+    {
+        Camera.main.transform.position = viewPositions[currentView];
+        GameObject.Find("MainInterface/NavigationButtons/Left").SetActive(true);
+        GameObject.Find("MainInterface/NavigationButtons/Right").SetActive(true);
+        GameObject.Find("MainInterface/NavigationButtons/Down").SetActive(true);
+        GameObject.Find("MainInterface/NavigationButtons/Up").SetActive(false);
+    }
+
+    private void ResetSelected()
+    {
+        GameObject eventSystem = GameObject.Find("EventSystem");
+        if(eventSystem != null)
+        {
+            eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
+        }
+    }
+}
---
+++ Assets/Scripts/MainInterfaceScript.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainInterfaceScript : MonoBehaviour {
+
+    GameObject ouijaBoard;
+    GameObject planchette;
+
+    Vector3 toPositionOuija;
+    Vector3 toPositionPlanchette;
+
+    bool ouijaSlide;
+    bool spiritTalking;
+
+    public float planchetteMoveSpeed;
+    public float ouijaMoveSpeed;
+
+    public float spiritThinkingTime;
+    private float spiritThinkingCooldown;
+
+    private int xPeriod = 1;
+    private int yPeriod = 1;
+    private int frameCountx = 1;
+    private int frameCounty = 1;
+
+
+    // Use this for initialization
+    void Start () {
+        ouijaBoard = GameObject.Find("MainInterface/OuijaBoard");
+        planchette = GameObject.Find("MainInterface/OuijaBoard/Planchette");
+
+        toPositionOuija = new Vector3(0, -261, 0);
+        spiritThinkingCooldown = spiritThinkingTime;
+
+        OnOuijaCall(true, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (ouijaSlide)
+        {
+            float speed = ouijaMoveSpeed * Time.deltaTime;
+            Vector3 ouijaBoardPisition = ouijaBoard.GetComponent<RectTransform>().localPosition;
+
+            ouijaBoard.GetComponent<RectTransform>().localPosition = Vector3.MoveTowards(ouijaBoardPisition, toPositionOuija, speed);
+            if (ouijaBoardPisition == toPositionOuija)
+            {
+                spiritTalking = true;
+                ouijaSlide = false;
+            }
+        }
+
+        if (spiritTalking)
+        {
+            if(spiritThinkingCooldown >= 0.0f)
+            {
+                spiritThinkingCooldown -= Time.deltaTime;
+
+                if (frameCountx == xPeriod)
+                {
+                    xPeriod = Random.Range(50, 200);
+                    frameCountx = 1;
+                }
+                if(frameCounty == yPeriod)
+                {
+                    yPeriod = Random.Range(50, 200);
+                    frameCounty = 1;
+                }
+
+                float xPosition = 100 * Mathf.Cos((Mathf.PI * 2) * frameCountx / xPeriod);
+                float yPosition = 100 * Mathf.Sin((Mathf.PI * 2) * frameCounty / yPeriod);
+                planchette.GetComponent<RectTransform>().localPosition = new Vector3(xPosition, yPosition, 0);
+
+                ++frameCountx;
+                ++frameCounty;
+            }
+            else
+            {
+                float speed = planchetteMoveSpeed * Time.deltaTime;
+                Vector3 planchettePos = planchette.GetComponent<RectTransform>().localPosition;
+
+                planchette.GetComponent<RectTransform>().localPosition = Vector3.MoveTowards(planchettePos, toPositionPlanchette, speed);
+                if(planchette.transform.position == toPositionPlanchette)
+                {
+                    spiritTalking = false;
+                }
+            }
+        }
+	}
+
+    public void OnOuijaCall(bool hasAnswer, bool isAffirmative)
+    {
+        if (hasAnswer)
+        {
+            if (isAffirmative)
+            {
+                toPositionPlanchette = new Vector3(-138, 114, 0);
+            }
+            else
+            {
+                toPositionPlanchette = new Vector3(178, 114, 0);
+            }
+            ouijaSlide = true;
+        }
+    }
+}
