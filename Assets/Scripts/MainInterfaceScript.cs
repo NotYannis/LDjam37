133361c255e0c6ac3ed3c8d3477a5a211fbfd8fd
Yannis Attard <yannis.attard@gmail.com>
2016-12-10 14:22:45 +0100

    Ouija board working

--- Assets/Scripts/MainInterfaceScript.cs
+++ Assets/Scripts/MainInterfaceScript.cs
@@ -10,14 +10,18 @@ public class MainInterfaceScript : MonoBehaviour {
     Vector3 toPositionOuija;
     Vector3 toPositionPlanchette;
 
-    bool ouijaSlide;
+    bool ouijaSlideOn;
     bool spiritTalking;
+    bool ouijaSlideOff;
 
     public float planchetteMoveSpeed;
     public float ouijaMoveSpeed;
 
     public float spiritThinkingTime;
     private float spiritThinkingCooldown;
+
+    public float ouijaStayTime;
+    private float ouijaStayCooldown;
 
     private int xPeriod = 1;
     private int yPeriod = 1;
@@ -32,29 +36,36 @@ public class MainInterfaceScript : MonoBehaviour {
 
         toPositionOuija = new Vector3(0, -261, 0);
         spiritThinkingCooldown = spiritThinkingTime;
+        ouijaStayCooldown = ouijaStayTime;
 
         OnOuijaCall(true, false);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (ouijaSlide)
+        //Slide the ouija board on the game
+        if (ouijaSlideOn)
         {
             float speed = ouijaMoveSpeed * Time.deltaTime;
-            Vector3 ouijaBoardPisition = ouijaBoard.GetComponent<RectTransform>().localPosition;
+            Vector3 ouijaBoardPosition = ouijaBoard.GetComponent<RectTransform>().localPosition;
 
-            ouijaBoard.GetComponent<RectTransform>().localPosition = Vector3.MoveTowards(ouijaBoardPisition, toPositionOuija, speed);
-            if (ouijaBoardPisition == toPositionOuija)
+            ouijaBoard.GetComponent<RectTransform>().localPosition = Vector3.MoveTowards(ouijaBoardPosition, toPositionOuija, speed);
+            if (ouijaBoardPosition == toPositionOuija)
             {
                 spiritTalking = true;
-                ouijaSlide = false;
+                ouijaSlideOn = false;
+                toPositionOuija = new Vector3(0, -500, 0);
             }
         }
 
+        //Move the planchette
         if (spiritTalking)
         {
             if(spiritThinkingCooldown >= 0.0f)
             {
+                ++frameCountx;
+                ++frameCounty;
+
                 spiritThinkingCooldown -= Time.deltaTime;
 
                 if (frameCountx == xPeriod)
@@ -68,12 +79,9 @@ public class MainInterfaceScript : MonoBehaviour {
                     frameCounty = 1;
                 }
 
-                float xPosition = 100 * Mathf.Cos((Mathf.PI * 2) * frameCountx / xPeriod);
-                float yPosition = 100 * Mathf.Sin((Mathf.PI * 2) * frameCounty / yPeriod);
+                float xPosition = 80 * Mathf.Cos((Mathf.PI * 2) * frameCountx / xPeriod);
+                float yPosition = 80 * Mathf.Sin((Mathf.PI * 2) * frameCounty / yPeriod);
                 planchette.GetComponent<RectTransform>().localPosition = new Vector3(xPosition, yPosition, 0);
-
-                ++frameCountx;
-                ++frameCounty;
             }
             else
             {
@@ -81,11 +89,40 @@ public class MainInterfaceScript : MonoBehaviour {
                 Vector3 planchettePos = planchette.GetComponent<RectTransform>().localPosition;
 
                 planchette.GetComponent<RectTransform>().localPosition = Vector3.MoveTowards(planchettePos, toPositionPlanchette, speed);
-                if(planchette.transform.position == toPositionPlanchette)
+                if(planchette.GetComponent<RectTransform>().localPosition == toPositionPlanchette)
                 {
                     spiritTalking = false;
+                    ouijaSlideOff = true;
+                    spiritThinkingCooldown = spiritThinkingTime;
                 }
             }
+        }
+
+
+        if (ouijaSlideOff)
+        {
+            if(ouijaStayCooldown >= 0.0f)
+            {
+                ouijaStayCooldown -= Time.deltaTime;
+            }
+            else
+            {
+                float speed = ouijaMoveSpeed * Time.deltaTime;
+                Vector3 ouijaBoardPosition = ouijaBoard.GetComponent<RectTransform>().localPosition;
+
+                ouijaBoard.GetComponent<RectTransform>().localPosition = Vector3.MoveTowards(ouijaBoardPosition, toPositionOuija, speed);
+                if (ouijaBoard.GetComponent<RectTransform>().localPosition == toPositionOuija)
+                {
+                    ouijaSlideOff = false;
+                    toPositionOuija = new Vector3(0, -261, 0);
+                    ouijaStayCooldown = ouijaStayTime;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnOuijaCall(false, false);
         }
 	}
 
@@ -101,7 +138,20 @@ public class MainInterfaceScript : MonoBehaviour {
             {
                 toPositionPlanchette = new Vector3(178, 114, 0);
             }
-            ouijaSlide = true;
+            ouijaSlideOn = true;
         }
+        else
+        {
+            toPositionPlanchette = new Vector3(0, -175, 0);
+            ouijaSlideOn = true;
+        }
+
+        //Initialize planchette position
+        xPeriod = Random.Range(50, 200);
+        yPeriod = Random.Range(50, 200);
+
+        float xPosition = 70 * Mathf.Cos((Mathf.PI * 2) * frameCountx / xPeriod);
+        float yPosition = 70 * Mathf.Sin((Mathf.PI * 2) * frameCounty / yPeriod);
+        planchette.GetComponent<RectTransform>().localPosition = new Vector3(xPosition, yPosition, 0);
     }
 }
