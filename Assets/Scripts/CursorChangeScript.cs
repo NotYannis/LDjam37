4f410cbbf81cd17c0c478671935df9a2bf868c16
Yannis Attard <yannis.attard@gmail.com>
2016-12-11 22:08:02 +0100

    Added cursor change on buttons

---
+++ Assets/Scripts/CursorChangeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class CursorChangeScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler// required interface when using the OnPointerEnter method.
+{
+    public Texture2D cursorHover;
+    public Texture2D cursorNormal;
+
+    //Do this when the cursor enters the rect area of this selectable UI object.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(cursorHover, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
